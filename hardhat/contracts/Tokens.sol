ef2e01a6b412b62cb4e126e5649071c84f11029d
paul <birnbaumpaul@yahoo.fr>
2022-10-21 18:31:12 +0200

    Tokens Contract and Deployment

--- hardhat/contracts/Tokens.sol
+++ hardhat/contracts/Tokens.sol
@@ -5,13 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
+/*
  *@Author: Paul Birnbaum.
  *@title: France Token.
  *@notice: Creation of the France Token.
  *@dev: Will create 10000 tokens and can mint more later.
  */
 contract France is ERC20 {
-  constructor() ERC20("France", "FR") {
+  constructor() ERC20("France", "FRA") {
+    _mint(msg.sender, 10000 * 10**decimals());
+  }
+
+  //@notice: Allow to mint more token when the user want to make a bet
+  function mint(address _address, uint256 _amount) external {
+    _mint(_address, _amount);
+  }
+}
+
+/*
+ *@title: Brasil Token.
+ *@notice: Creation of the Brasil Token.
+ *@dev: Will create 10000 tokens and can mint more later.
+ */
+contract Brasil is ERC20 {
+  constructor() ERC20("Brasil", "BRA") {
     _mint(msg.sender, 10000 * 10**decimals());
   }
 
