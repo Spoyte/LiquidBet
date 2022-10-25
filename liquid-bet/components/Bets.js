ad40526240e83a5437bcf4a2eb8ad87c1644ae8d
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2022-10-25 17:43:18 +0100

    feat:  implemented some new designs

--- liquid-bet/components/Bets.js
+++ liquid-bet/components/Bets.js
@@ -3,10 +3,14 @@ import styles from '../styles/Home.module.css'
 const Bets = () => {
     return (
         <div className={styles.bets}>
-            <p>Your Bets</p>
-            <p>France - Brasil</p>
-            <p>Bet Amount - 2MATIC</p>
-            <button>PLACE BET</button>
+            <p className={styles.title}>BETSLIP</p>
+            <div className={styles.slipdetails}>
+                <p>France - Brasil</p>
+                <p>Full Time Result</p>
+                <p><span>1</span><span className={styles.slipOdds}>3.20</span></p>
+            </div>
+            <p className={styles.betAmount}>Bet Amount - 2MATIC</p>
+            <button className={styles.betBTN}>PLACE BET</button>
         </div>
       );
 }
--- liquid-bet/components/TopMatch.js
+++ liquid-bet/components/TopMatch.js
@@ -5,12 +5,24 @@ const TopMatch = () => {
         <div className={styles.topmatch}>
             <p>⚽ Top Football Matches!</p>
             <div>
-                <p>Flags</p>
+                <p>World cup - FIFA</p>
+                <div>
+                    <span>Fra flag</span>
+                    <span>France</span>
+                    <span>1 <span>3.20</span></span>
+                </div>
+                <div>Today 20:45</div>
+                <div>
+                    <span>Bra flag</span>
+                    <span>Brazil</span>
+                    <span></span>
+                </div>
+                {/* <p>Flags</p>
                 <p>France-Brasil</p>
                 <div>
                     <span>Bet France</span>
                     <span>Bet Brasil</span>
-                </div>
+                </div> */}
             </div>
 
         </div>
